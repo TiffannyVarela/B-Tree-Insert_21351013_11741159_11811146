35968b1193b7781c4e9254604abf98d4b14d498d
TiffannyVarela <tiffanny_varela529@unitec.edu>
2020-12-06 15:27:12 -0600

    1527

--- src/b/tree/insert_11741159_11811146_/Admin_B_Tree.java
+++ src/b/tree/insert_11741159_11811146_/Admin_B_Tree.java
@@ -28,6 +28,7 @@ public class Admin_B_Tree {
         public int clave;
         public String contenido;
         public Empleado hijos[];
+        public int Claves [];
 
         public Nodo(Empleado x) {
             this.padre = null;
@@ -35,10 +36,72 @@ public class Admin_B_Tree {
             this.l = null;
             this.clave = x.getCodigo();
             this.contenido = x.toString();
-            this.hijos = new Empleado [max];
+            this.hijos = new Empleado [orden];
+            this.Claves = new int [max];
+        }   
+
+        public Nodo getPadre() {
+            return padre;
+        }
+
+        public void setPadre(Nodo padre) {
+            this.padre = padre;
+        }
+
+        public Nodo getR() {
+            return r;
+        }
+
+        public void setR(Nodo r) {
+            this.r = r;
+        }
+
+        public Nodo getL() {
+            return l;
+        }
+
+        public void setL(Nodo l) {
+            this.l = l;
+        }
+
+        public int getClave() {
+            return clave;
+        }
+
+        public void setClave(int clave) {
+            this.clave = clave;
+        }
+
+        public String getContenido() {
+            return contenido;
+        }
+
+        public void setContenido(String contenido) {
+            this.contenido = contenido;
+        }
+
+        public Empleado[] getHijos() {
+            return hijos;
+        }
+
+        public void setHijos(Empleado[] hijos) {
+            this.hijos = hijos;
         }
         
         
         
+    }
+    
+    public void Crear(){
+        root = null;
+        
+    }
+    
+    public boolean hoja (Nodo x){
+        boolean resp = false;
+        if (x.getHijos().length==0) {
+            resp = true;
+        }
+        return resp;
     }
 }
